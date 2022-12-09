7fc6d34be2eb8ce302fc6ffac627392fd4315df0
Nikolay-zhukau <koliajukov@gmail.com>
2022-12-09 10:27:03 +0300

    Finished module 4 / task 4

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryAssignment {
     public void assignAndPrintBasedOnWhichBigger(int first, int second) {
+        int firstOne = 10;
+        int secondOne = -10;
+        int result = (first >second) ? firstOne:secondOne;
+        System.out.println(result);
     }
 }
